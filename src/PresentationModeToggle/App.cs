4b80d6f6f531c3a113e8de51d33bb368c854394c
Jiri Cincura <jiri@x2develop.com>
2015-05-23 14:54:04 +0200

    Initial version of PresentationModeToggle.

---
+++ src/PresentationModeToggle/App.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace PresentationModeToggle
+{
+	class App
+	{
+		static void Main(string[] args)
+		{
+			var state = SHQueryUserNotificationState();
+			if (state != null)
+			{
+				var toggleResult = state == Interop.QUERY_USER_NOTIFICATION_STATE.QUNS_PRESENTATION_MODE
+					? ExecutePresentationSettings("stop")
+					: ExecutePresentationSettings("start");
+				if (!toggleResult)
+				{
+					ShowProblem("Unable to toggle Presentation Mode.");
+				}
+			}
+			else
+			{
+				ShowProblem("Unable to get Presentation Mode status.");
+			}
+		}
+
+		static Interop.QUERY_USER_NOTIFICATION_STATE? SHQueryUserNotificationState()
+		{
+			Interop.QUERY_USER_NOTIFICATION_STATE state;
+			return Interop.SHQueryUserNotificationState(out state) == Interop.S_OK
+				? state
+				: (Interop.QUERY_USER_NOTIFICATION_STATE?)null;
+		}
+
+		static bool ExecutePresentationSettings(string action)
+		{
+			var psi = new ProcessStartInfo();
+			psi.FileName = "PresentationSettings.exe";
+			psi.Arguments = string.Format("/{0}", action);
+			try
+			{
+				using (var process = Process.Start(psi))
+				{ }
+			}
+			catch
+			{
+				return false;
+			}
+			return true;
+		}
+
+		static void ShowProblem(string text)
+		{
+			MessageBox.Show(text, string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+		}
+	}
+}
---
+++ src/PresentationModeToggle/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PresentationModeToggle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PresentationModeToggle")]
+[assembly: AssemblyCopyright("Copyright © Jiri {x2} Cincura 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("419F5C6F-1863-4411-865B-F41C9B7CFFFB")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
